9fad92aa2e7a802e5686ad40e578e7b1edde6ab7
Matt-ey3d <matthew.ytk@outlook.pt>
2025-09-29 10:27:57 +0100

    Add files via upload

---
+++ PrimeiroProgramaInsultuosoDeSempre.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"A tua mãe é tão burra\n";
+    std::cout << "Tão burra\n";
+    std::cout << "Que ela foi despedida da fábrica dos M&Ms\n";
+    std::cout << "Por deitar fora os Ws\n";
+    std::cout << "\n";
+    std::cout << "A stora Clara é fixe\n";
+    std::cout << "Tenho pena da turma de animais que ela tem de ensinar";
+    return 0;
+}
