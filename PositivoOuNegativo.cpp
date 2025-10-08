8d4424be52c1a003e412f93587802804d17f8495
Mateus Silvestre <matthew.ytk@outlook.pt>
2025-10-08 18:23:41 +0100

    Add files via upload

---
+++ PositivoOuNegativo.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	int num;
+
+	do
+	{
+		cout << "Escreve um n�mero:\n";
+		cin >> num;
+
+		if (num > 0)
+		{
+			cout << "O n�mero � positivo!\n";
+		}
+		else
+		{
+			cout << "O n�mero � negativo!\n";
+		}
+	} while (num != 0);
+	cout << "Levas uma galheta t�s aqui t�s na Sib�ria\n";
+	cout << "Mas tu pensas que isto � o qu�?\n";
+	cout << "Pensas que podes gozar comigo assim inculto mal-feito?\n";
+	cout << "Só por causa das merdas estragaste o programa e eu fui dizer que era negativo\n";
+	cout<<"Mas tu pensas que mandas aqui?\n";
+	cout<<"OH filho duma grande cabra\n";
+	cout <<"Já que tu andas a pedi-las\n";
+	cout<<"Vou insultar-te como o Parvo insulta o Diabo no Auto da Barca do Inferno\n";
+	cout<<"Sapateiro da Candosa\n";
+	cout<<"Antrecosto de carrapato\n";
+	cout<<"Caga no sapato\n";
+	cout<<"Filho da grande aleivosa\n";
+	cout<<"Tua mulher é tinhosa\n";
+	cout<<"E há de parir um sapo\n";
+	cout<<"Chantado num guardanapo\n";
+	cout<<"Neto da cagarrinhosa\n";
+	cout<<"Furta-cebolas\n";
+	cout<<"Excomungado nas igrejas\n";
+	cout<<"Burrela, CORNUDO SEJAS!!!!\n";
+	cout<<"Toma o pão que te caiu XDDDD\n";
+	cout<<"A mulher que te fugiu\n";
+	cout<<"Para a ilha da Madeira\n";
+	cout<<"Cornudo até à mangueira 8==D\n";
+	cout<<"TOMA O PÃO QUE TE CAIU XDDDDDDDDD\n";
+	cout<<"Lanço-te uma pulha\n";
+	cout<<"Pica naquela\n";
+	cout<<"Caga na vela\n";
+	cout<<"Cabeça de grulha\n";
+	cout<<"Perna de cigarra velha\n";
+	cout<<"Caganita de coelha\n";
+	cout<<"Pelourinho da Pampulha\n";
+	cout<<"Mija na agulha mija na agulha\n";
+	cout<<"Toma lá\n";
+	cout<<"Já crashei o teu computador\n";
+	cout<<"É o que acontece quando te metes com quem não deves\n";
+	cout<<"Finalmente eu mando-te para o caralho\n";
+	cout<<"\n";
+	
+	
+
+	return 0;
+}
