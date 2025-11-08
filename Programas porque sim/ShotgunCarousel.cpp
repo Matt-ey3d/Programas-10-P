989f8e8e7c9a01aafed1ea56f90b37274577a217
Mateus Silvestre <matthew.ytk@outlook.pt>
2025-11-08 16:44:52 +0000

    Add files via upload

--- Programas porque sim/ShotgunCarousel.cpp
+++ Programas porque sim/ShotgunCarousel.cpp
@@ -3,34 +3,229 @@
 #include <locale.h>
 #include <cstdlib>
 #include <time.h>
+#include <windows.h>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 	srand(time(0));
 
-	int vitimas[6] = {}, i, j;
-	string pessoas[10] = { "EU TENHO UMA FAM√çLIA!", "Pressiona o bot√£o! PRESSIONA O BOT√ÉO!!", "Por favor deixa-me viver!!!", "...", "NUNCA MAIS TE CHATEIO NA VIDA! PROMETO!!", "AAAAAAAAAAAAAAAA!!!!!!!!!", "Ah, obrigado.", "Eu dou-te dinheiro, fama, TUDO! POR FAVOR N√ÉO ME MATES!!!", "N√£o me mates! Quem √© que vai dar comida ao meu c√£o???","J√° n√£o tinha amigos mesmo." };
-	string nomes[10] = { "Joaquim Costa", "Fernando Anast√°cio", "Beatriz Sousa", "Mateus Silvestre", "Afonso Pacheco", "Cau√£ Galliac", "Bruno Silva", "Carlos Miguel", "Afonso Santo", "Rodrigo Galante"};
+	int vitimas[6] = {}, i, j, teimoso = 0;
+	string OK;
+	auto inicio = steady_clock::now();
+	auto tempo = seconds(13);
+	string falas[10] = {
+		"EU TENHO UMA FAMÕLIA!",//1
+		"Pressiona o bot„o! PRESSIONA O BOT√O!!",//2
+		"Por favor deixa-me viver!!!",//3
+		"...",//4
+		"NUNCA MAIS TE CHATEIO NA VIDA! PROMETO!!",//5
+		"AAAAAAAAAAAAAAAA!!!!!!!!!",//6
+		"Ah, muito obrigado.",//7
+		"Eu dou-te dinheiro, fama, TUDO! POR FAVOR N√O ME MATES!!!",//8
+		"N„o me mates! Quem È que vai dar comida ao meu c„o???",//9
+		"J· n„o tinha amigos mesmo."//10
+	};
+	string pessoas[10] = {
+		"Joaquim Costa",//1
+		"Fernando Anast·cio",//2
+		"Beatriz Sousa",//3
+		"Mateus Silvestre",//4
+		"Afonso Pacheco",//5
+		"Cau„ Galliac",//6
+		"Bruno Silva",//7
+		"Carlos Miguel",//8
+		"Afonso Santo",//9
+		"Rodrigo Galante"//10
+	};
+
 
 	for (i = 0; i < 6; ++i)
 	{
 		vitimas[i] = rand() % 10;
+		for (j = 0; j < i; ++j)
+		{
+			if (vitimas[i] == vitimas[j])
+			{
+				--i;
+			}
+		}
 	}
-	cout << "Ol√°. Eu quero jogar um jogo.\n";
+
+
+	cout << "Ol·. Eu quero jogar um jogo." << endl;
+	cout << "(Enter)";
 	cin.ignore();
 	system("cls");
 	
+	for (i = 0; i < 4; ++i)
+	{
+		cout << "  \ O /  " << endl;
+		cout << " O \ / O " << endl;
+		cout << "----*----" << endl;
+		cout << " O / \ O " << endl;
+		cout << "  / O \  " << endl;
+		Sleep(300);
+		system("cls");
+		cout << " O  |  O " << endl;
+		cout << "___ | ___" << endl;
+		cout << " O \*/ O " << endl;
+		cout << "___/|\___" << endl;
+		cout << " O  |  O " << endl;
+		Sleep(300);
+		system("cls");
+	}
+
+	cout << "Esta coisa esquisita que acabou de aparecer no teu ecr„ era suposto ser um carrosel." << endl;
+	cout << "(Enter)";
+	cin.ignore();
+	system("cls");
+	cout << "Todas as vezes que jogas um jogo, tens sempre a opÁ„o de matar ou deixar viver o monstro ‡ tua frente." << endl;
+	cout << "(Enter)";
+	cin.ignore();
+	system("cls");
+	cout << "Todas as vezes que jogas um jogo, tens sempre a opÁ„o de matar ou deixar viver o monstro ‡ tua frente." << endl;
+	cout << "Agora ter·s de escolher, de entre os teus amigos, quem matas e quem deixas viver." << endl;
+	cout << "(Enter)";
+	cin.ignore();
+	system("cls");
+	cout << "6 dos teus amigos est„o presos no carrosel," << endl;
+	Sleep(1500);
+	cout << "mas apenas 3 podem sair." << endl;
+	cout << "(Enter)";
+	cin.ignore();
+	system("cls");
+
+	for (i = 0; i < 4; ++i)
+	{
+		cout << "  \ O /  " << endl;
+		cout << " O \ / O " << endl;
+		cout << "----*----" << endl;
+		cout << " O / \ O " << endl;
+		cout << "  / O \  " << endl;
+		cout << endl << "Lembra-te que eles n„o s„o os monstros dos jogos;" << endl;
+		Sleep(300);
+		system("cls");
+		cout << " O  |  O " << endl;
+		cout << "___ | ___" << endl;
+		cout << "O  \*/  O" << endl;
+		cout << "___/|\___" << endl;
+		cout << " O  |  O " << endl;
+		cout << endl << "Lembra-te que eles n„o s„o os monstros dos jogos;" << endl;
+		Sleep(300);
+		system("cls");
+	}
+	for (i = 0; i < 5; ++i)
+	{
+		cout << "  \ O /  " << endl;
+		cout << " O \ / O " << endl;
+		cout << "----*----" << endl;
+		cout << " O / \ O " << endl;
+		cout << "  / O \  " << endl;
+		cout << endl << "Lembra-te que eles n„o s„o os monstros dos jogos;" << endl;
+		cout << "aquilo que tu escolheres vai ter consequÍncias." << endl;
+		Sleep(300);
+		system("cls");
+		cout << " O  |  O " << endl;
+		cout << "___ | ___" << endl;
+		cout << "O  \*/  O" << endl;
+		cout << "___/|\___" << endl;
+		cout << " O  |  O " << endl;
+		cout << endl << "Lembra-te que eles n„o s„o os monstros dos jogos;" << endl;
+		cout << "aquilo que tu escolheres vai ter consequÍncias." << endl;
+		Sleep(300);
+		system("cls");
+	}
+
+	cout << "Tens 15 segundos para salvares cada um." << endl;
+	cout << "(Enter)";
+	cin.ignore();
+	system("cls");
+	cout << "Tens 13 segundos para salvares cada um." << endl;
+	cout << "QUE O JOGO COMECE." << endl;
+	Sleep(1000);
 	do
 	{
-		cout << " ";
+		if (teimoso > 2)
+		{
+			cout << "\33[31m";
+		}
+		cout << "(COME«AR? [´okª])" << endl;
+		cin >> OK;
+		++teimoso;
+		if (teimoso > 5 && teimoso < 10 && OK != "ok")
+		{
+			for (i = 0; i < 67; ++i)
+			{
+				cout << "COVARDE" << endl;
+				Sleep(5);
+			}
+		}
+		if (teimoso > 9 && OK != "ok" )
+		{
+			for (i = 0; i < 67; ++i)
+			{
+				for (j = 0; j < 101; ++j)
+				{
+					cout << "COVARDE ";
+				}
+				cout << endl;
+			}
+		}
+	} while (OK != "ok");
+
+	system("cls");
+
+	cout << "\33[0m";
+
+	for (i = 0; i < 6; ++i)
+	{
+		for (j = 0; j < 6; ++j)
+		{
+			cout << "A GIRAR..." << endl;
+			cout << "  \ O /  " << endl;
+			cout << " O \ / O " << endl;
+			cout << "----*----" << endl;
+			cout << " O / \ O " << endl;
+			cout << "  / O \  " << endl;
+			Sleep(300);
+			system("cls");
+			cout << "A GIRAR..." << endl;
+			cout << " O  |  O " << endl;
+			cout << "___ | ___" << endl;
+			cout << "O  \*/  O" << endl;
+			cout << "___/|\___" << endl;
+			cout << " O  |  O " << endl;
+			Sleep(300);
+			system("cls");
+		}
+		cout << endl;
+		cout << "  \ O /  " << endl;
+		cout << " O \ / O " << endl;
+		cout << "----*----" << endl;
+		cout << " O / \ O " << endl;
+		cout << "  / \33[31mO\33[0m \  " << endl;
+		Sleep(1000);
 		system("cls");
-		++j
-	} while (j < 20);
-	
 
-
+		while (steady_clock::now() - inicio < tempo);
+		{
+			for (j = 13; j > 0; --j)
+			{
+				cout << pessoas[vitimas[i]] << endl << "´" << falas[vitimas[i]] << "ª" << endl;
+				cout << j;
+				Sleep(1000);
+				system("cls");
+			}
+			if (cin.ignore())
+			{
+				break;
+			}
+		}
+	}
 	return 0;
 }
