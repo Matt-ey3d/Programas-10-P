d8e84654320d33895c0e4faece8c30944ffe88dd
Mateus Silvestre <matthew.ytk@outlook.pt>
2026-01-22 14:23:35 +0000

    Add files via upload

--- C++/Funções/ComprasEmMoedas.cpp
+++ C++/Funções/ComprasEmMoedas.cpp
@@ -7,52 +7,45 @@
 
 using namespace std;
 
-int i, troco_teste, distribuição[8];
+int i, distribuição[8], multiplicadores[8] = { 200, 100, 50, 20, 10, 5, 2, 1 }, preço, pagamento = 0; /*
+	"i" para loops, moedas dadas, multiplicadores de cada moeda (200 para moedas de 2 euros, 100 para moedas de 1 euro, 50 para moedas de 50 cêntimos, etc.),
+preço da compra e pagamento do cliente em cêntimos
+*/
 
-int verificarMoneyDado(int moeda) //transformar, em cêntimos, o dinheiro dado pelo cliente
+int transformarMoneyDado(int moeda) //transformar, em cêntimos, o dinheiro dado pelo cliente
 {
-	int pagamento = 0; //pagamento (em cêntimos)
-
-	//Afonso Espanhol sugere fazer outro array em vez do switch case
-
-	switch (i)
-	{
-	case 0:
-		pagamento += moeda * 200;
-		break;
-	case 1:
-		pagamento += moeda * 100;
-		break;
-	case 2:
-		pagamento += moeda * 50;
-		break;
-	case 3:
-		pagamento += moeda * 20;
-		break;
-	case 4:
-		pagamento += moeda * 10;
-		break;
-	case 5:
-		pagamento += moeda * 5;
-		break;
-	case 6:
-		pagamento += moeda * 2;
-		break;
-	case 7:
-		pagamento += moeda * 1;
-		break;
-	}
-	return pagamento;
+	return moeda * multiplicadores[i];
 }
 
-int vendaFeita()
+void vendaFeita() //verificar se a venda foi efetuada
 {
-	if (troco_teste < 0)
+	int troco[8] = { 0 }, resto;
+
+	resto = pagamento - preço;
+	if (resto < 0) //se o dinheiro não chegar para a compra
 	{
-		for (i = 0; i < 7; ++i)
+		for (i = 0; i < 8; ++i)
 		{
 			cout << distribuição[i] << " ";
 		}
+		cout << endl;
+	}
+	else
+	{
+		for (i = 0; i < 8; ++i)
+		{
+			if (resto >= multiplicadores[i]) //se o preço chegar para a moeda em multiplicadores[i]
+			{
+				++troco[i]; //mais 1 moeda para o seu lugar respetivo
+				resto -= multiplicadores[i]; //retira-se o dinheiro do troco ao preço
+				--i; //volta a repetir para verificar se é preciso mais moedas do mesmo tipo
+			}
+		}
+		for (i = 0; i < 8; ++i)
+		{
+			cout << troco[i] << " ";
+		}
+		cout << endl;
 	}
 }
 
@@ -60,15 +53,14 @@ void main()
 {
 	setlocale(LC_ALL, "Portuguese");
 
-	int E, C, troco[8], preço, pagamento = 0; //euros, cêntimos, moedas dadas, moedas recebidas, preço do artigo (em cêntimos)
+	int E, C; //euros, cêntimos, moedas dadas, moedas recebidas, preço do artigo (em cêntimos)
 
 	cin >> E >> C;
-	preço = (E * 100) + C;
-	for (i = 0; i < 7; ++i)
+	preço = (E * 100) + C; //preço da compra em cêntimos
+	for (i = 0; i < 8; ++i)
 	{
-		cin >> distribuição[i];
-		pagamento+=verificarMoneyDado(distribuição[i]);
+		cin >> distribuição[i]; //ler moedas
+		pagamento += transformarMoneyDado(distribuição[i]); //adicionar cada moeda ao pagamento, em cêntimos, em forma de função
 	}
-	troco_teste = pagamento - preço;
-	vendaFeita();
+	vendaFeita(); //verificar se a venda pode ser feita e efetuá-la
 }
